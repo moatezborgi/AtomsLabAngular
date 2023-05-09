603addbac6c7cc610fc384286d63150115ff1b0d
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-05-09 23:30:01 +0100

    Delete AtomsLabApp/src directory

--- AtomsLabApp/src/app/Model/UserManagementModels/AuthenticationRequest.ts
+++
@@ -1,9 +0,0 @@
-export class AuthenticationRequest {
-  username!: string;
-  password!: string;
-  public constructor(username :string,password :string){
-    this.password=password
-    this.username=username
-
-  }
-}
