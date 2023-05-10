c3f0b9a9bb60164a7eb3206e61c0aa2ba0beba63
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-05-10 09:22:42 +0100

    Delete src directory

--- src/app/Model/RequestAnalysisModels/Appointment.ts
+++
@@ -1,9 +0,0 @@
-import {Inscription} from "./Inscription";
-import {RequestAnalysis} from "./RequestAnalysis";
-
-export interface Appointment {
-  numappointement: string;
-  date_appointment: string;
-  inscription: Inscription;
-  requestAnalysis: RequestAnalysis;
-}
