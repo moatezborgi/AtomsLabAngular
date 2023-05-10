0c28089c78d68f291d4dde8047cfa457f44e1e39
Yassine Ben Salah <99485994+yassinesprit@users.noreply.github.com>
2023-05-10 10:31:08 +0200

    Delete src directory

--- src/app/Model/UserManagementModels/ResesPasswordRequest.ts
+++
@@ -1,9 +0,0 @@
-export class ResesPasswordRequest {
-  token!: string;
-  password!: string;
-
-  constructor(token: string, password: string) {
-    this.token = token;
-    this.password = password;
-  }
-}
