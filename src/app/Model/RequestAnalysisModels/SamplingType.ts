01a61c33a015dc55ec3e4dd515e6024a645f9619
Yassine Ben Salah <99485994+yassinesprit@users.noreply.github.com>
2023-05-10 10:10:49 +0200

    Delete src directory

--- src/app/Model/RequestAnalysisModels/SamplingType.ts
+++
@@ -1,8 +0,0 @@
-import {Act} from "./Act";
-import {ActRequestAnalysisSampling} from "./ActRequestAnalysisSampling";
-
-export interface SamplingType {
-  idSampling: number;
-  descSampling: string;
-
-}
